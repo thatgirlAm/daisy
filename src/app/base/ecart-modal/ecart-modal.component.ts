2670d5817b882d313a1629ecad0d38386f0893a6
thatgirlAm2 <amaelle.diop@etu.estia.fr>
2024-08-09 12:54:14 +0000

    dev: ecart modal

--- src/app/base/ecart-modal/ecart-modal.component.ts
+++ src/app/base/ecart-modal/ecart-modal.component.ts
@@ -1,30 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Ecart } from '../ecart';
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-ecart-modal',
   templateUrl: './ecart-modal.component.html',
   styleUrls: ['./ecart-modal.component.css'],
-  standalone: true,
-  imports: [CommonModule, NgIf],
-  providers: [Ecart]
+   standalone : true,
+   imports: [NgIf, NgFor, CommonModule]
 })
-export class EcartModalComponent implements OnInit {
+export class EcartModalComponent {
   @Input() isOpen: boolean = false;
-  ecart !:Ecart | null ; 
-  constructor(ecart : Ecart) {
-    this.ecart = new Ecart();
-  }
+  @Input() ecart!: Ecart;
 
-
-  ngOnInit(): void {}
-
-  openModal(): void {
-    this.isOpen = true;
-  }
-
-  closeModal(): void {
+  closeModal() {
     this.isOpen = false;
   }
 }
--- src/app/base/ecarts/ecarts.component.ts
+++ src/app/base/ecarts/ecarts.component.ts
@@ -1,65 +1,96 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoaderComponent } from '../loader/loader.component';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { ApiService } from '../api.service';
-import { ToastrService } from 'ngx-toastr'; 
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 // import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 // import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-import { registerLocaleData } from '@angular/common';
 import { Ecart } from '../ecart';
 // import { sendModalComponent } from '../send-modal/send-modal.component';
 import { EcartModalComponent } from '../ecart-modal/ecart-modal.component';
-import { IgxDateRangePickerComponent } from 'igniteui-angular';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-ecarts',
   templateUrl: './ecarts.component.html',
   styleUrls: ['./ecarts.component.css'],
   imports:[
-    // HeaderComponent,
-    // MatTableModule,
-    // LateralMenuComponent,
+    EcartModalComponent, 
     LoaderComponent,
     FormsModule,
     CommonModule,
     NgIf,
     NgFor,
-    // sendModalComponent,
-    // MatPaginatorModule, 
-
-     EcartModalComponent
   ],
-    standalone: true,  
-
+  standalone: true 
 })
-export class EcartsComponent implements OnInit {
+export class EcartsComponent implements OnInit{
+  @ViewChild(EcartModalComponent) ecartModal!: EcartModalComponent;
+  selectedEcart!: Ecart;
+  isModalOpen: boolean = false;
   filteredEcarts: Ecart[] = [];
+  itemsPerPage: number = 10;
   dateFilter: string = '';
   archivedFilter: boolean = false;
-  itemsPerPage: number = 10;
-  selectedEcart!: Ecart | null ;
   ecarts: Ecart[] = [];
-  isModalOpen: boolean = false; 
   loaded: boolean = false;
   currentPage: number = 1;
   url='http://127.0.0.1:8000/api/';
-  ecartModal !: EcartModalComponent ; 
 
-  // @ViewChild(MatPaginator) paginator!: MatPaginator;
-  constructor(
-    private router: Router,
-    private http: HttpClient,
-    private api: ApiService,
-    private toastr : ToastrService  ) {}
+  constructor(private toastr:ToastrService, private api:ApiService) 
+    { 
 
-  ngOnInit() {
+    }
+
+  ngOnInit(): void {
     this.loadEcarts();
-    
   }
 
+paginatedEcarts(): Ecart[] {
+    const startIndex = this.currentPage * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.filteredEcarts.slice(startIndex, endIndex);
+  }
+  applyFilters() {
+    let tempEcarts = this.ecarts;
+
+    if (this.dateFilter) {
+      tempEcarts = tempEcarts.filter(ecart =>
+        new Date(ecart.created_at).toDateString() === new Date(this.dateFilter).toDateString()
+      );
+    }
+
+    this.filteredEcarts = tempEcarts;
+
+    // if (this.paginator) {
+    //   this.paginator.length = this.filteredEcarts.length;
+    //   this.paginator.pageIndex = 0;
+    // }
+  }
+
+totalPages(): number {
+    return Math.ceil(this.filteredEcarts.length / this.itemsPerPage);
+  }
+
+  async openModal(ecart:Ecart) {
+    this.selectedEcart = ecart;
+    console.log(this.selectedEcart);
+    console.log(this.ecartModal);
+    if (this.ecartModal) {
+      this.ecartModal.isOpen = true;
+      console.log(this.ecartModal);
+      this.isModalOpen = true;
+    } else {
+      console.log('issue');
+    }
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    if (this.ecartModal) {
+      this.ecartModal.isOpen = false;
+    }
+  }
   loadEcarts() {
     this.loaded = false;
     this.api.getData(this.url+'stats').subscribe({
@@ -79,54 +110,5 @@ export class EcartsComponent implements OnInit {
     });
     this.applyFilters(); 
   }
-  applyFilters() {
-    let tempEcarts = this.ecarts;
-
-    if (this.dateFilter) {
-      tempEcarts = tempEcarts.filter(ecart =>
-        new Date(ecart.created_at).toDateString() === new Date(this.dateFilter).toDateString()
-      );
-    }
-
-    this.filteredEcarts = tempEcarts;
-
-    // if (this.paginator) {
-    //   this.paginator.length = this.filteredEcarts.length;
-    //   this.paginator.pageIndex = 0;
-    // }
-  }
-
-  // onPageChange(event: PageEvent) {
-  //   this.currentPage = event.pageIndex + 1;
-  //   this.itemsPerPage = event.pageSize;
-  // }
   
-  get paginatedEcarts() {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.filteredEcarts.slice(startIndex, endIndex);
-  }
-  
-  get totalPages(): number {
-    return Math.ceil(this.filteredEcarts.length / this.itemsPerPage);
-  }
-
-  async openModal(ecart: Ecart) {
-    
-    this.selectedEcart = await ecart;
-    this.ecartModal = new EcartModalComponent(ecart);
-    console.log(this.selectedEcart);
-    if (this.ecartModal) {
-      this.ecartModal.openModal();
-      console.log(this.ecartModal);
-      this.isModalOpen = true ;
-    } else {
-      console.log('issue');
-    }
-  }
-
-  closeModal() {
-    this.isModalOpen = false ;
-   
-  }
 }
